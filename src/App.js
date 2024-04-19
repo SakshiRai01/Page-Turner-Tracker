5715e23be0f925bd6adf625d7b747d0c8b921984
SakshiRai01 <0111sakshirai@gmail.com>
2024-04-19 20:08:04 +0530

    updated editing and deleting the reading list

--- src/App.js
+++ src/App.js
@@ -15,20 +15,26 @@ function App() {
         fetchBooks();
     }, []);
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle };
+                return { ...book, ...response.data };
             }
             return book;
         });
+        
         setBooks(updatedBooks);
     };
 
